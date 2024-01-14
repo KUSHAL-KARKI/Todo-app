3963b159f9a43285d93186f9bcea6e907afe5d40
KUSHAL-KARKI <kushalkarki1344@gmail.com>
2024-01-14 23:02:45 +0545

    Initian commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTodo = () => {
+    if (task.trim() !== '') {
+      setTodos([...todos, task]);
+      setTask('');
+    }
+  };
+
+  const removeTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center justify-center h-screen cursor-pointer">
+      <div className="bg-gray-100 p-8 shadow-lg rounded-2xl w-96">
+      <h1 className=" font-bold text-4xl mb-4 py-2 text-center 
+                      border-2 mx-11 rounded-xl 
+                     border-gray-500 shadow-lg">ToDo App</h1>
+      <div className="flex my-5">
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Enter task"
+          className=" flex-grow rounded-l-3xl p-2 border-collapse drop-shadow-lg"
+        />
+        <button onClick={addTodo} className=" bg-slate-500 shadow-lg
+         text-white rounded-r-3xl  px-4
+         hover:bg-zinc-600 active:bg-zinc-700">Add</button>
+      </div>
+      <ul className="">
+        {todos.map((todo, index) => (
+          <li key={index}
+         className="flex justify-between items-center p-2 border-b border-black">
+            {todo}
+            <button onClick={() => removeTodo(index)} className="mt-3
+             bg-slate-500 shadow-lg text-white rounded-2xl px-3
+         hover:bg-zinc-600 active:bg-zinc-700">Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
     </div>
   );
 }
